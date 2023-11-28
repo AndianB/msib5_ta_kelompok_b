84b4ccd51ab551af7c6dcbfc41a17369dd7f902b
MrRavent <gkgalang@gmail.com>
2023-11-28 19:24:18 +0700

    menghapus gimmick

--- filmrev/public/movie-details.php
+++ filmrev/public/movie-details.php
@@ -83,20 +83,7 @@
               <p>Lorem ipsum dolor sit amet, consecetur adipiscing elseddo eiusmod tempor.There are many
                 variations of passages of lorem
                 Ipsum available, but the majority have suffered alteration in some injected humour.</p>
-              <div class="movie-details-prime">
-                <ul>
-                  <li class="share"><a href="#"><i class="fas fa-share-alt"></i> Share</a></li>
-                  <li class="streaming">
-                    <h6>Prime Video</h6>
-                    <span>Streaming Channels</span>
-                  </li>
-                  <li class="watch"><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="btn popup-video"><i class="fas fa-play"></i> Watch Now</a></li>
-                </ul>
-              </div>
             </div>
-          </div>
-          <div class="movie-details-btn">
-            <a href="img/poster/movie_details_img.jpg" class="download-btn" download="">Download <img src="fonts/download.svg" alt=""></a>
           </div>
         </div>
       </div>
@@ -104,89 +91,6 @@
     <!-- movie-details-area-end -->
 
     <!-- episode-area -->
-    <section class="episode-area episode-bg" data-background="img/bg/episode_bg.jpg">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-8">
-            <div class="movie-episode-wrap">
-              <div class="episode-top-wrap">
-                <div class="section-title">
-                  <span class="sub-title">ONLINE STREAMING</span>
-                  <h2 class="title">Watch Full Episode</h2>
-                </div>
-                <div class="total-views-count">
-                  <p>2.7 million <i class="far fa-eye"></i></p>
-                </div>
-              </div>
-              <div class="episode-watch-wrap">
-                <div class="accordion" id="accordionExample">
-                  <div class="card">
-                    <div class="card-header" id="headingOne">
-                      <button class="btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        <span class="season">Season 2</span>
-                        <span class="video-count">5 Full Episodes</span>
-                      </button>
-                    </div>
-                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                      <div class="card-body">
-                        <ul>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 1 - The World Is Purple</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span></li>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 2 - Meaner Than Evil</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span></li>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 3 - I Killed a Man Today</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span></li>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 4 - Cowboys and Dreamers</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span></li>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 5 - Freight Trains and Monsters</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="card">
-                    <div class="card-header" id="headingTwo">
-                      <button class="btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        <span class="season">Season 1</span>
-                        <span class="video-count">5 Full Episodes</span>
-                      </button>
-                    </div>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                      <div class="card-body">
-                        <ul>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 1 - The World Is Purple</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span>
-                          </li>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 2 - Meaner Than Evil</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span></li>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 3 - I Killed a Man Today</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span>
-                          </li>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 4 - Cowboys and Dreamers</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span>
-                          </li>
-                          <li><a href="https://www.youtube.com/watch?v=R2gbPxeNk2E" class="popup-video"><i class="fas fa-play"></i> Episode 5 - Freight Trains and Monsters</a> <span class="duration"><i class="far fa-clock"></i> 28 Min</span></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="episode-img">
-              <img src="img/images/episode_img.jpg" alt="">
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <div class="movie-history-wrap">
-              <h3 class="title">About <span>History</span></h3>
-              <p>Lorem ipsum dolor sit amet, consecetur adipiscing elseddo eiusmod tempor.There are many variations of passages of lorem
-                Ipsum available, but the majority have suffered alteration in some injected humour.There are many variations of passages
-                of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised
-                words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
-                there isn't anything errassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to
-                repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of
-                over 200 Latin words, combined with a handful</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     <!-- episode-area-end -->
 
     <!-- tv-series-area -->
@@ -295,26 +199,6 @@
     <!-- tv-series-area-end -->
 
     <!-- newsletter-area -->
-    <section class="newsletter-area newsletter-bg" data-background="img/bg/newsletter_bg.jpg">
-      <div class="container">
-        <div class="newsletter-inner-wrap">
-          <div class="row align-items-center">
-            <div class="col-lg-6">
-              <div class="newsletter-content">
-                <h4>Trial Start First 30 Days.</h4>
-                <p>Enter your email to create or restart your membership.</p>
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <form action="#" class="newsletter-form">
-                <input type="email" required placeholder="Enter your email">
-                <button class="btn">get started</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     <!-- newsletter-area-end -->
 
   </main>
