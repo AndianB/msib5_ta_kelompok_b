b42f23d9aa2c2952cb6d127f1bc2dafacc5c1471
ArliyandoRandaLinggi <arliyando2804@gmail.com>
2023-11-28 12:58:03 +0700

    template user

---
+++ filmrev/public/contact.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>Filmrev</title>
+  <meta name="description" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png" />
+  <!-- Place favicon.ico in the root directory -->
+
+  <!-- CSS here -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" />
+  <link rel="stylesheet" href="css/animate.min.css" />
+  <link rel="stylesheet" href="css/magnific-popup.css" />
+  <link rel="stylesheet" href="css/fontawesome-all.min.css" />
+  <link rel="stylesheet" href="css/owl.carousel.min.css" />
+  <link rel="stylesheet" href="css/flaticon.css" />
+  <link rel="stylesheet" href="css/odometer.css" />
+  <link rel="stylesheet" href="css/aos.css" />
+  <link rel="stylesheet" href="css/slick.css" />
+  <link rel="stylesheet" href="css/default.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/responsive.css" />
+</head>
+
+<body>
+  <!-- preloader -->
+  <div id="preloader">
+    <div id="loading-center">
+      <div id="loading-center-absolute">
+        <img src="img/preloader.svg" alt="" />
+      </div>
+    </div>
+  </div>
+  <!-- preloader-end -->
+
+  <!-- Scroll-top -->
+  <button class="scroll-top scroll-to-target" data-target="html">
+    <i class="fas fa-angle-up"></i>
+  </button>
+  <!-- Scroll-top-end-->
+
+  <!-- header-area -->
+  <?php include "template/header.php" ?>
+  <!-- header-area-end -->
+
+  <!-- main-area -->
+  <main>
+    <!-- breadcrumb-area -->
+    <section class="breadcrumb-area breadcrumb-bg" data-background="img/bg/contact_us_bg.jpg">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="breadcrumb-content">
+              <h2 class="title">Contact Us</h2>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item">
+                    <a href="index.php">Home</a>
+                  </li>
+                  <li class="breadcrumb-item active" aria-current="page">
+                    Contact
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- breadcrumb-area-end -->
+
+    <!-- contact-area -->
+    <section class="contact-area contact-bg" data-background="img/bg/contact_bg.jpg">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-8 col-lg-7">
+            <div class="contact-form-wrap">
+              <div class="widget-title mb-50">
+                <h5 class="title">Contact Form</h5>
+              </div>
+              <div class="contact-form">
+                <form action="#">
+                  <div class="row">
+                    <div class="col-md-6">
+                      <input type="text" placeholder="You Name *" />
+                    </div>
+                    <div class="col-md-6">
+                      <input type="email" placeholder="You  Email *" />
+                    </div>
+                  </div>
+                  <input type="text" placeholder="Subject *" />
+                  <textarea name="message" placeholder="Type Your Message..."></textarea>
+                  <button class="btn">Send Message</button>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-4 col-lg-5">
+            <div class="widget-title mb-50">
+              <h5 class="title">Information</h5>
+            </div>
+            <div class="contact-info-wrap">
+              <p>
+                <span>Find solutions :</span> to common problems, or get help
+                from a support agent industry's standard .
+              </p>
+              <div class="contact-info-list">
+                <ul>
+                  <li>
+                    <div class="icon">
+                      <i class="fas fa-map-marker-alt"></i>
+                    </div>
+                    <p><span>Address :</span> W38 Park Road New York</p>
+                  </li>
+                  <li>
+                    <div class="icon"><i class="fas fa-phone-alt"></i></div>
+                    <p><span>Phone :</span> (09) 123 854 365</p>
+                  </li>
+                  <li>
+                    <div class="icon"><i class="fas fa-envelope"></i></div>
+                    <p><span>Email :</span> support@movflx.com</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- contact-area-end -->
+  </main>
+  <!-- main-area-end -->
+
+  <!-- footer-area -->
+  <?php include "template/footer.php" ?>
+  <!-- footer-area-end -->
+
+  <!-- JS here -->
+  <script src="js/vendor/jquery-3.6.0.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+  <script src="js/imagesloaded.pkgd.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.odometer.min.js"></script>
+  <script src="js/jquery.appear.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/ajax-form.js"></script>
+  <script src="js/wow.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
