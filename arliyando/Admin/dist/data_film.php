4411e6dbb4f91eec6cd9d087227671b5c370bf94
khansaln <khansa.luthfiyah2000@gmail.com>
2023-11-30 07:58:46 +0700

    Up

--- arliyando/Admin/dist/data_film.php
+++ arliyando/Admin/dist/data_film.php
@@ -26,6 +26,35 @@
     <!-- Icons css -->
     <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
 </head>
+<script>
+    function konfirmasiHapus(GenreID) {
+
+        swal({
+                title: "Apakah Kamu yakin?",
+                text: "Data akan terhapus!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+                    // Redirect ke halaman Proses-hapus-data.php jika konfirmasi disetujui
+                    swal({
+                        title: "Good job!",
+                        text: "Data terhapus!",
+                        icon: "success",
+                    });
+                    setTimeout(function() {
+                        window.location.href = "proses_hapus_film.php?FilmID=" + FilmID;
+                    }, 2000);
+                } else {
+                    swal("Data Batal Di hapus!", {
+                        icon: "info",
+                    });
+                }
+            });
+    }
+</script>
 
 <body>
     <!-- Begin page -->
@@ -183,168 +212,58 @@
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h4 class="header-title">Data</h4>
-
+                                    <h4 class="header-title">Data Film</h4>
                                 </div>
                                 <div class="card-body">
+                                    <?php
+                                    include '../config/koneksi.php';
+                                    $query = mysqli_query($conn, "SELECT * from film as f join genre as g on g.GenreID=f.GenreID join kategori_umur as k on k.KategoriID=f.KategoriID ;");
+                                    ?>
                                     <table id="datatable-buttons" class="table table-striped dt-responsive nowrap w-100">
 
                                         <thead>
                                             <tr>
-                                                <th>Name</th>
-                                                <th>Position</th>
-                                                <th>Office</th>
-                                                <th>Age</th>
-                                                <th>Start date</th>
-                                                <th>Salary</th>
-                                                <th>Action</th>
+                                                <th>Film ID</th>
+                                                <th>Judul</th>
+                                                <th>Gambar</th>
+                                                <th>Link Trailer</th>
+                                                <th>Synopsis</th>
+                                                <th>Rating</th>
+                                                <th>Tanggal Rilis</th>
+                                                <th>Durasi</th>
+                                                <th>Sutradara</th>
+                                                <th>Genre</th>
+                                                <th>Kategori</th>
+                                                <th>Aksi</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>Tiger Nixon</td>
-                                                <td>System Architect</td>
-                                                <td>Edinburgh</td>
-                                                <td>61</td>
-                                                <td>2011/04/25</td>
-                                                <td>$320,800</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
                                             </tr>
-                                            <tr>
-                                                <td>Garrett Winters</td>
-                                                <td>Accountant</td>
-                                                <td>Tokyo</td>
-                                                <td>63</td>
-                                                <td>2011/07/25</td>
-                                                <td>$170,750</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Ashton Cox</td>
-                                                <td>Junior Technical Author</td>
-                                                <td>San Francisco</td>
-                                                <td>66</td>
-                                                <td>2009/01/12</td>
-                                                <td>$86,000</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Cedric Kelly</td>
-                                                <td>Senior Javascript Developer</td>
-                                                <td>Edinburgh</td>
-                                                <td>22</td>
-                                                <td>2012/03/29</td>
-                                                <td>$433,060</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Airi Satou</td>
-                                                <td>Accountant</td>
-                                                <td>Tokyo</td>
-                                                <td>33</td>
-                                                <td>2008/11/28</td>
-                                                <td>$162,700</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Brielle Williamson</td>
-                                                <td>Integration Specialist</td>
-                                                <td>New York</td>
-                                                <td>61</td>
-                                                <td>2012/12/02</td>
-                                                <td>$372,000</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Herrod Chandler</td>
-                                                <td>Sales Assistant</td>
-                                                <td>San Francisco</td>
-                                                <td>59</td>
-                                                <td>2012/08/06</td>
-                                                <td>$137,500</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Rhona Davidson</td>
-                                                <td>Integration Specialist</td>
-                                                <td>Tokyo</td>
-                                                <td>55</td>
-                                                <td>2010/10/14</td>
-                                                <td>$327,900</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Colleen Hurst</td>
-                                                <td>Javascript Developer</td>
-                                                <td>San Francisco</td>
-                                                <td>39</td>
-                                                <td>2009/09/15</td>
-                                                <td>$205,500</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Sonya Frost</td>
-                                                <td>Software Engineer</td>
-                                                <td>Edinburgh</td>
-                                                <td>23</td>
-                                                <td>2008/12/13</td>
-                                                <td>$103,600</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Jena Gaines</td>
-                                                <td>Office Manager</td>
-                                                <td>London</td>
-                                                <td>30</td>
-                                                <td>2008/12/19</td>
-                                                <td>$90,560</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <td>Quinn Flynn</td>
-                                                <td>Support Lead</td>
-                                                <td>Edinburgh</td>
-                                                <td>22</td>
-                                                <td>2013/03/03</td>
-                                                <td>$342,000</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-warning btn-sm">Edit</a>
-                                                    <a href="#" onclick="return confirm('Delete data?')" class="btn btn-danger btn-sm">Delete</a>
-                                                </td>
-                                            </tr>
+                                            <?php
+                                            if (mysqli_num_rows($query) > 0) {
+                                                $no = 1;
+                                                while ($data = mysqli_fetch_array($query)) {
+                                            ?>
+                                                    <tr>
+                                                        <td> <?php echo $data["FilmID"] ?></td>
+                                                        <td> <?php echo $data["Judul"] ?></td>
+                                                        <td> <?php echo $data["Gambar"] ?></td>
+                                                        <td> <?php echo $data["Link_Trailer"] ?></td>
+                                                        <td> <?php echo $data["Synopsis"] ?></td>
+                                                        <td> <?php echo $data["Rating"] ?></td>
+                                                        <td> <?php echo $data["Tanggal_Release"] ?></td>
+                                                        <td> <?php echo $data["Durasi_Film"] ?></td>
+                                                        <td> <?php echo $data["Sutradara"] ?></td>
+                                                        <td> <?php echo $data["GenreID"] ?></td>
+                                                        <td> <?php echo $data["KategoriID"] ?></td>
+                                                        <td> <?php echo $data["GenreID"] ?></td>
+                                                        <td>
+                                                            <a href="edit_film.php?FilmID=<?php echo $data["FilmID"] ?>" class="btn btn-warning btn-sm">Edit</a>
+                                                            <a href="proses_hapus_film.php?FilmID=<?php echo $data["FilmID"] ?>" onclick="konfirmasiHapus( . $data['FilmID'] . )" class="btn btn-danger btn-sm">Delete</a>
+                                                        </td>
+                                                    </tr>
+                                                <?php } ?>
+                                            <?php } ?>
                                         </tbody>
                                     </table>
 
@@ -369,6 +288,7 @@
                 <!-- end Footer -->
 
 </body>
+
 <!-- Vendor js -->
 <script src="assets/js/vendor.min.js"></script>
 
@@ -392,6 +312,7 @@
 
 <!-- App js -->
 <script src="assets/js/app.min.js"></script>
+
 
 
 
