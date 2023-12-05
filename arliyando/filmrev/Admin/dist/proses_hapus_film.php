3865a63f180db4bb92c71c5b8e4a0cd0655387a8
ArliyandoRandaLinggi <arliyando2804@gmail.com>
2023-12-05 20:22:47 +0700

    ubah

--- arliyando/filmrev/Admin/dist/proses_hapus_film.php
+++ arliyando/filmrev/Admin/dist/proses_hapus_film.php
@@ -3,4 +3,4 @@ include '../config/koneksi.php';
 
 $result = mysqli_query($conn, "DELETE from film where `FilmID` = '$_GET[FilmID]'");
 
-header("Location:data_film.php");
+header("Location:index.php");
