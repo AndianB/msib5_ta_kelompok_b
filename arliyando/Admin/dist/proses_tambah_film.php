59adcb4c91a2b95fb87f095c3b2f08bf0fc147e7
MrRavent <gkgalang@gmail.com>
2023-12-05 19:22:28 +0700

    upload gambar ke dua tempat berbeda

--- arliyando/Admin/dist/proses_tambah_film.php
+++ arliyando/Admin/dist/proses_tambah_film.php
@@ -4,8 +4,14 @@ include '../config/koneksi.php';
 $target_dir = "assets/images/uploads/";
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+// Pindahkan file pertama ke lokasi pertama
 move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
 
+// Menyalin file ke lokasi kedua
+$second_target_dir = "../../filmrev/public/img/posters/";
+$second_target_file = $second_target_dir . basename($_FILES["image"]["name"]);
+copy($target_file, $second_target_file);
 
 $filmid = $_POST["FilmID"];
 $judul = $_POST["Judul"];
@@ -22,3 +28,4 @@ $kategori = $_POST["Kategori"];
 $result = mysqli_query($conn, "INSERT INTO `film` (`FilmID`, `Judul`, `Gambar`, `Link_Trailer`, `Synopsis`, `Rating`, `Tanggal_Release`, `Durasi_Film`, `Sutradara`, `GenreID`, `KategoriID`) VALUES ('$filmid', '$judul', '$gambar', '$link', '$sinopsis', '$rating', '$tgl_rilis', '$durasi', '$sutradara', '$genre', '$kategori');");
 
 header("Location:data_film.php");
+?>
